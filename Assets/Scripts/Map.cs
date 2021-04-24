65ed792c0102b5d40d08234708222d0d21a4fe31
John Zhen <john.zhen24@myhunter.cuny.edu>
2021-04-24 16:21:28 -0400

    fixed trash generation on map

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -6,6 +6,8 @@ using UnityEngine.SceneManagement;
 
 public class Map : MonoBehaviour
 {
+    const int NUM_OF_TRASH_ON_MAP = 35;
+    const float BORDER = 1.4f;
     private SpriteRenderer map;
     private Vector2 dimensions;
 
@@ -14,20 +16,21 @@ public class Map : MonoBehaviour
     {
         Scene scene = SceneManager.GetActiveScene();
         GameObject[] gameobjs = scene.GetRootGameObjects();
-        GameObject bg = gameobjs[1];
+        GameObject bg = gameobjs[0];
         map = bg.GetComponent<SpriteRenderer>();
         dimensions = map.size;
         Debug.Log(dimensions);
         string[] trashPaths = {"Assets/Trash/plasticbag.png", "Assets/Trash/plasticbottle.png", "Assets/Trash/sodacan.png"};
-        for(int i = 0; i < 10; i++)
+        for(int i = 0; i < NUM_OF_TRASH_ON_MAP; i++)
         {
             GameObject trash = new GameObject();
-            float x  = Random.Range((-1*dimensions.x)+1, dimensions.x);
-            float y = Random.Range((-1*dimensions.y)+1, dimensions.y);
-            trash.transform.position = new Vector2(x,y);
-            SpriteRenderer trashRenderer = trash.AddComponent<SpriteRenderer>();
+            trash.name = "Trash";
             int trashIndex = Random.Range(0,3);
             Texture2D tex = CreateTexture(trashPaths[trashIndex]);
+            float x  = Random.Range((-1*dimensions.x)+BORDER, dimensions.x-BORDER);
+            float y = Random.Range((-1*dimensions.y)+BORDER, dimensions.y-BORDER);
+            trash.transform.position = new Vector2(x,y);
+            SpriteRenderer trashRenderer = trash.AddComponent<SpriteRenderer>();
             Sprite spr = Sprite.Create(tex, new Rect(0.0f, 0.0f, tex.width, tex.height), new Vector2(x/2, y/2), 100.0f);
             trashRenderer.sprite = spr;
         }
