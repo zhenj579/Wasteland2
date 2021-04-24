6fc61c81a383a7cee0ae29918910e8ac5152b3d3
Bexzod Abduvaliev <50254948+BexzodA@users.noreply.github.com>
2021-04-23 23:36:14 -0400

    Made the camera follow the player.

---
+++ Assets/PlayerFollower.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollower : MonoBehaviour
+{
+
+    private Transform cam;
+    public Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(player.position);
+        cam.position = new Vector3(player.position.x, player.position.y, cam.position.z);
+    }
+}
