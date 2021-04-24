0c031e2d7cce3203bbf738ab7cc024dbf5e493a0
timzhen38 <44072887+timzhen38@users.noreply.github.com>
2021-04-24 17:38:19 -0400

    fixed some boundaries

--- Assets/Scripts/StayInside.cs
+++ Assets/Scripts/StayInside.cs
@@ -7,6 +7,6 @@ public class StayInside : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -15f, 15.5f), Mathf.Clamp(transform.position.y, -7f, 7f), transform.position.z);
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -15.5f, 15.5f), Mathf.Clamp(transform.position.y, -7.5f, 7.5f), transform.position.z);
     }
 }
