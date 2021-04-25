6ade11da42bd79581b5483ff8db1ba5cc0138e94
Bexzod Abduvaliev <50254948+BexzodA@users.noreply.github.com>
2021-04-25 11:58:51 -0400

    Made the trash more animated.

--- Assets/Scripts/Trash.cs
+++ Assets/Scripts/Trash.cs
@@ -5,18 +5,27 @@ using UnityEngine;
 public class Trash : MonoBehaviour
 {
     public AudioSource sfx;
+    private Transform trans;
     private SpriteRenderer renderer;
+    private float sum = 0f;
     // Start is called before the first frame update
     void Start()
     {
         sfx = GetComponent<AudioSource>();
         renderer = GetComponent<SpriteRenderer>();
+        trans = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    private void FixedUpdate()
+    {
+        float sin = Mathf.Sin(sum) / 200;
+        sum += 2*Time.deltaTime;
+        trans.position = new Vector3(trans.position.x, trans.position.y + sin, trans.position.z);
     }
 
     void OnTriggerEnter2D(Collider2D other)
